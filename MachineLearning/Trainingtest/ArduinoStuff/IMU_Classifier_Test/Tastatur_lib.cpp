51f225cc7bbd93ceb8471e07de7dc79553099dba
Escape9002 <75317197+Escape9002@users.noreply.github.com>
2021-12-09 14:11:13 +0100

    new tasttaur layout
    deprerssion

--- MachineLearning/Trainingtest/ArduinoStuff/IMU_Classifier_Test/Tastatur_lib.cpp
+++ MachineLearning/Trainingtest/ArduinoStuff/IMU_Classifier_Test/Tastatur_lib.cpp
@@ -10,13 +10,19 @@ void printToKeyboard(String s){
   }
 }
 
-/*
- void pressToKeyboard(String s){
-  keyboard.key_code(s, 0x00);
+
+ int pressToKeyboard(int  s){ //, uint8_t  modifier
+  //keyboard.key_code(s, modifier);
+  keyboard..printf(s);
  }
 
    //RIGHT_ARROW,      
     //LEFT_ARROW,        
    // DOWN_ARROW,         
-   // UP_ARROW,           
- */
+   // UP_ARROW, 
+
+
+//    {0x2a, 0},          /* BS  */  /* Keyboard Delete (Backspace) */
+//    {0x2b, 0},          /* TAB */  /* Keyboard Tab */
+ //   {0x28, 0},          /* LF  */  /* Keyboard Return (Enter) */
+ 
