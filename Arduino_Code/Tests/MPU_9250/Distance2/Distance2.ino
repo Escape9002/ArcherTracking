1f86bd61bb12ca23355f0839ab7c5542426b1c56
Escape9002 <antonio.rehwinkel@schiller-og.de>
2021-12-28 13:49:19 +0100

    Commented the general working style

--- Arduino_Code/Tests/MPU_9250/Distance2/Distance2.ino
+++ Arduino_Code/Tests/MPU_9250/Distance2/Distance2.ino
@@ -1,28 +1,16 @@
 /*
-  Brian R Taylor
-  brian.taylor@bolderflight.com
+   Measures the distance via constance acceleration formulas or integral formulas.
+   If wanted one can output the Data as CSV in the Serial Monitor.
 
-  Copyright (c) 2021 Bolder Flight Systems Inc
+   Conifureable:
+   CSV-Output
+   HZ of the Sensor
+   used formula
 
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files (the “Software”), to
-  deal in the Software without restriction, including without limitation the
-  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-  sell copies of the Software, and to permit persons to whom the Software is
-  furnished to do so, subject to the following conditions:
-
-  The above copyright notice and this permission notice shall be included in
-  all copies or substantial portions of the Software.
-
-  THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-  IN THE SOFTWARE.
+   BolderFlight Library used
 */
-#define CSVDISTANCE 1 //toggle CSVDISTANCE mode
+//---------------------------------------------------toggle CSVDISTANCE mode
+#define CSVDISTANCE 1
 
 #define DISABLE_MPU9250_FIFO
 #include "mpu9250.h"
@@ -50,7 +38,9 @@ void setup() {
     Serial.println("Error initializing communication with IMU");
     while (1) {}
   }
-  /* Set the sample rate divider */   // 9 works ok || 
+  // -------------------------------------------------------------HZ of the sensor
+  //(9 works ok, 0 zero is to high (rauschen), 19 is too low(ungenügende Daten))
+  /* Set the sample rate divider */
   if (!imu.ConfigSrd(9)) {
     Serial.println("Error configured SRD");
     while (1) {}
@@ -84,7 +74,13 @@ void loop() {
     aY = imu.accel_y_mps2() - aYOFF;
     //aZ = imu.accel_z_mps2() - aZOFF;
 
+    //---------------------------------------------------which formula to use
+    // function takes the acceleration and true Hz to work.
     constDistance_m(aY, hz);
+    //constDistance_cm(aY, hz);
+
+    //integralDistance_m(aY, hz);
+    //integralDistance_cm(aY, hz);
 
 
   }
