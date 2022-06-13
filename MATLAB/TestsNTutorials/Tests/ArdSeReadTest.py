9a0eba4fe4d4b4c3f083f5a2c92c4c15aa19ffbb
Escape9002 <75317197+Escape9002@users.noreply.github.com>
2022-06-13 23:58:26 +0200

    working simulation
    the results.mot needs to be DELETED and Recreated for each simulation as far as known now

--- MATLAB/TestsNTutorials/Tests/ArdSeReadTest.py
+++ MATLAB/TestsNTutorials/Tests/ArdSeReadTest.py
@@ -1,26 +1,24 @@
-from asyncio.windows_events import NULL
+from numpy import unicode_
 import serial
-
-import csv
-import json
 
 ser = serial.Serial('COM4', 115200, timeout = 1)
 ser.flushInput()
 
-kopfzeile = "DataRate=10.000000\nDataType=Quaternion\nversion=3\nOpenSimVersion=4.3-2021-08-27-4bc7ad9\nendheader\ntime\tpelvis_imu" #\thumerus_l_imu\ttorso_imu\tulna_l_imu
-timer = 500
+kopfzeile = "DataRate=10.000000\nDataType=Quaternion\nversion=3\nOpenSimVersion=4.3-2021-08-27-4bc7ad9\nendheader\ntime\tpelvis_imu\n" #\thumerus_l_imu\ttorso_imu\tulna_l_imu
+
+sec = 60
+timer = int(sec / (1/10))
 
 print(timer * (1/10))
 
-with open('adressen_schreiben.txt', 'w', encoding='utf-8') as data:
+with open('data.sto', 'w') as data:
     data.write(kopfzeile)
 
     for i in range(timer):
         line = ser.readline()   # read a byte
         if line:
-            string = line.decode()  # convert the byte string to a unicode string
-            #num = int(string) # convert the unicode string to an int
-            print(str(i) +"\t"+string)
-            data.write(str(i) +"\t"+ string)
-
-        
+            string = (line.decode().replace("\n","").replace("b",""))  # convert the byte string to a unicode string and replace \n with emptyness
+            #print(str(i) +"\t"+string)
+            string = str(i) +"\t"+string
+            print(string)
+            data.write(string)
