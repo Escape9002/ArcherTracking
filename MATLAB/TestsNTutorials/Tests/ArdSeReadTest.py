d8f22d648163d01c70a2514f86b7c623b6a03e07
Escape9002 <75317197+Escape9002@users.noreply.github.com>
2022-06-19 22:12:37 +0200

    multi Sensor Read
    and new data

--- MATLAB/TestsNTutorials/Tests/ArdSeReadTest.py
+++ MATLAB/TestsNTutorials/Tests/ArdSeReadTest.py
@@ -1,12 +1,12 @@
 from numpy import unicode_
 import serial
 
-ser = serial.Serial('COM4', 115200, timeout = 1)
+ser = serial.Serial('COM9', 115200, timeout = 1)
 ser.flushInput()
 
-kopfzeile = "DataRate=100.000000\nDataType=Quaternion\nversion=3\nOpenSimVersion=4.3-2021-08-27-4bc7ad9\nendheader\ntime\tpelvis_imu\n" #\thumerus_l_imu\ttorso_imu\tulna_l_imu
+kopfzeile = "DataRate=50.000000\nDataType=Quaternion\nversion=3\nOpenSimVersion=4.3-2021-08-27-4bc7ad9\nendheader\ntime\thumerus_l_imu\n" #\thumerus_l_imu\ttorso_imu\tulna_l_imu
 
-sec = 60
+sec = 10
 timer = int(sec / (1/10))
 msg = []
 
@@ -15,7 +15,7 @@ print(timer * (1/10))
 with open('data.sto', 'w') as data:
     data.write(kopfzeile)
 
-    for i in range(timer):
+    for i in range(timer+1):
         line = ser.readline()   # read a byte
         if line:
             string = (line.decode().replace("\n","").replace("b",""))  # convert the byte string to a unicode string and replace \n with emptyness
