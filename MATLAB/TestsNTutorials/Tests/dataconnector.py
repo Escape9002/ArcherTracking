d9996da24dba24bc15c45ee42fc54f2aa1c3914f
Escape9002 <75317197+Escape9002@users.noreply.github.com>
2022-06-22 19:55:26 +0200

    new Data and programms
    running full body program and full data capture

---
+++ MATLAB/TestsNTutorials/Tests/dataconnector.py
@@ -0,0 +1,36 @@
+import csv
+
+def load_data(header, fileName):
+    dataArray1 = []
+    outputArray = []
+    with open(fileName, newline='') as csvfile:
+        file = csv.reader(csvfile, delimiter=' ')
+
+        i=0
+        while i < header:
+            i = i+ 1
+            next(file)
+        
+        for row in file:
+            for data in row:
+                dataArray1.append(data.split("\t"))
+
+        for data in dataArray1:
+            outputArray.append(data[1] + "\t" + data[2])
+    print(outputArray)
+    return outputArray
+
+def write_data(filename,kopfzeile,data1, data2):
+    with open(filename, 'w') as data:
+        data.write(kopfzeile)
+        i = 0
+        while i < len(data1):
+            msg = str(i) + "\t" + str(data1[i]) + "\t" + str(data2[i]) + "\n"
+            i = i+1
+            data.write(msg)
+
+huefte = load_data(6, 'data_huefte.sto')
+arm = load_data(6,'data_arm_r.sto')
+
+write_data('appendData.sto',
+    "DataRate=50.000000\nDataType=Quaternion\nversion=3\nOpenSimVersion=4.3-2021-08-27-4bc7ad9\nendheader\ntime\ttorso_imu\tpelvis_imu\thumerus_r_imu\tulna_r_imu\n",huefte,arm)
